75f78be60e86701be7df50cff4a163e36b1dc9d9
Igor Souza Martins <nulldreams@gmail.com>
2018-02-26 17:22:33 -0300

    v1.0

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
     <div class="container has-text-centered">
       <h1 class="title">About Me</h1>
       <p>
-        My name is Igor Souza Martins, I’m a software developer based in Maringá - PR, Brazil. I build modern applications for the web!.
+        My name is Igor Souza Martins, I’m a software developer based in Maringá - PR, Brazil. I build modern applications for the web!
       </p>
     </div>
   </section>
